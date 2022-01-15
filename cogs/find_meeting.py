d87a2340fdde05dd53e2fac9377f4fe3b2bb7d4e
pgeorgoulis <ice18390063@uniwa.gr>
2022-01-15 05:02:09 +0200

    Now uses time/date objects and can calculate start and end time of the session

--- cogs/find_meeting.py
+++ cogs/find_meeting.py
@@ -18,19 +18,18 @@ class Find_meeting(commands.Cog):
 
         with open(utils.get_filename(), 'r', newline="") as csvfile:
             reader = csv.reader(csvfile, delimiter=",")
-
+            date_dictionary1 = {}
+            date_dictionary2 = {}
             #Get the first line
             first_row = next(reader)
             #remove the first element which is the name
             first_row.pop(0)
             #first_days = set(row)
-            date_dictionary1 = {}
             date_dictionary1 = utils.date_parser(first_row)
 
             #Get the second line
             second_row = next(reader)
             second_row.pop(0)
-            date_dictionary2 = {}
             date_dictionary2 = utils.date_parser(second_row)
             #Common elements of the first two
             common_days = date_dictionary1.keys() & date_dictionary2.keys()
@@ -51,7 +50,7 @@ class Find_meeting(commands.Cog):
                 common_days = common_days.intersection(row_set)
 
                 print("$$$$$$")
-                print(common_days)
+                print(f'the common_days are {common_days}\n')
 
 
         #read the whole file and store it to a list. Each user/line is a dictionary of dates
@@ -65,30 +64,22 @@ class Find_meeting(commands.Cog):
                 dictionary = utils.date_parser(row)
                 lines.append(dictionary)
 
-        print(lines)
-
         for date in common_days:
             start_times = []
             end_times = []
             for i in range(line_count):
-                print(lines[i].get(date))
+                print(f'the lines[i].get(date) are {lines[i].get(date)}\n')
                 time = lines[i].get(date)
-                start_time, end_time = utils.split_time(time)
-                start_times.append(start_time)
-                end_times.append(end_time)
+                start_times.append(time[0])
+                end_times.append(time[1])
+                print(f'the start_times.append(time[0]) end_times.append(time[1]) are {time[0]} {time[1]}\n')
             #now we have all the start and end times from all the users for that date
             #The real starting time will be the maximum value of the start_times list
             #The real ending time will be the minimum value of the end_times list
-            start = utils.max(start_times)
-            ## TODO: this sucks. Find a better way to compare times. Search if a date object exists
-            s_hour = start.split(":")
-            s_h = int(s_hour)
-            end = utils.min(end_times)
-            e_hour = end.split(":")
-            e_h = int(e_hour)
-            timeframe = e_h - s_h
-            if end-timeframe > 4 :
-                await ctx.send(f'Possible session at {date} {start} : {end}')
+            max = utils.max_time(start_times)
+            print(f'max(start time) {max.time_to_string()}\n')
+            min = utils.min_time(end_times)
+            print(f'min(end time) {min.time_to_string()}\n')
 
 
 def setup(client):
