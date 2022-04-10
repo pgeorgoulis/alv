799a7244c8cf6276052c9bf638daa2e42605c68a
pgeorgoulis <ice18390063@uniwa.gr>
2022-04-10 14:18:39 +0300

    Added purge funtion

--- cogs/find_meeting.py
+++ cogs/find_meeting.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import MissingPermissions
 from discord.utils import get
+from datetime import date
 import csv
 import utils
 
@@ -10,17 +11,47 @@ class Find_meeting(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    #This function gets called every time the find_meeting does.
+    #It deletes from the file all the dates that have passed
+    def purge(self, channel_users):
+        print(f'The users in this channel are: {channel_users}')
+
+        cur_day = date.today().day
+        cur_month = date.today().month
+
+        new_file = []
+        for user in channel_users:
+            users_dates, exit, message = utils.get_users_dates(user)
+            temp_list = []
+            temp_list.append(user)
+            if exit == 0:
+                for entry in users_dates:
+                    month = int(entry.get_month())
+                    day = int(entry.get_day())
+                    if month == cur_month:
+                        if day > cur_day:
+                            temp_list.append(entry.get_full_date())
+                    elif month > cur_month:
+                        temp_list.append(entry.get_full_date())
+            #TODO replace all the writting to the file here and in the show_and_remove
+            #with a seperate function. Also, optimize this solution
+            new_file.append(temp_list)
+
+        with open(utils.get_filename(), 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(new_file)
+
+
     @commands.command()
     @commands.has_permissions(administrator=True)   #Raises some subclass CommandError
     async def find_meeting(self, ctx):
 
         channel_users = []
-        print("#######")
         for m in ctx.channel.members:
             channel_users.append(str(m))
         #Remove the bots name from the list
         channel_users.remove("Alv#3487")
-        print(channel_users)
+        self.purge(channel_users)
 
         #Fetch the dates for all the users
 
