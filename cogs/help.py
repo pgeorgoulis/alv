7f795d97936495ad345dd554812ed9e9f2e02838
pgeorgoulis <porfyrios21@gmail.com>
2022-11-12 17:37:50 +0200

    minor changes

--- cogs/help.py
+++ cogs/help.py
@@ -9,26 +9,41 @@ class Help(commands.Cog):
     @commands.group(invoke_without_command=True)
     async def help(self, ctx):
         embed = discord.Embed(title="Help", description="Use !help <command> for extended information on a command", color = ctx.author.color)
-
-        embed.add_field(name="Session scheduling", value="add, remove, show, find_meeting")
+        embed.add_field(name="<command>", value="The following commands are available at this time", inline=False)
+        embed.add_field(name="Session scheduling", value="add, remove, show, find_meeting, stats")
         embed.add_field(name="Moderation", value="delete")
         await ctx.send(embed=embed)
 
     @help.command()
     async def add(self, ctx):
         em = discord.Embed(title="Add", description="Adds a date available for a session", color=ctx.author.color)
-        em.add_field(name="**Syntax**", value="!add to call the command, <date> (<time> - <time>) OR <date> (<string>) for each date entered. To enter multiple dates, seperate them with \n (enter)", inline=False)
-        em.add_field(name="<date>", value="Each <date> must follow the format of dd/mm i.e 25/12", inline=False)
-        em.add_field(name="<time>", value="Each <time> must follow the format of hh:mm i.e 12:56 (24h format)", inline=False)
-        em.add_field(name="<string>", value=("The available values for <string> are: day, morning, noon, night. "
+        em.add_field(name="**How to use**", value=("1. Use !add to call the command\n"
+                                                    "2. After you see the apropriate reply from alv, enter your dates using the following format:\n" 
+                                                    "   <date> (<time> - <time>)\n  OR\n    <date> (<string>)\n" 
+                                                    "3. Wait for alv to reply to your message confirming the sucessfull addition of the dates\n\n"
+                                                    "If you are unsure or you made a mistake you can use the commands !show and !delete to see or change your entries"), inline=False)
+        em.add_field(name="<date>", value="This field represents a day and a month.They must follow the format of *dd/mm i.e 25/2*", inline=False)
+        em.add_field(name="<time>", value="This field represents a set of hours and minutes. Each <time> must follow the format of *hh:mm i.e 12:56 (24h format)*", inline=False)
+        em.add_field(name="<string>", value=("This field represents a pre-set time frame. The available values for <string> are: *day*, *morning*, *noon*, *night*. "
                                             "\nEach string has an assigned start and end time."
                                             "\nday ->\t(9:00-23:59)\nmorning ->\t(11:30-15:30)\nnoon ->\t(15:30-20:30)\nnight ->\t(20:30-23:59)"), inline=False)
+        em.add_field(name="Examples", value=("Some correct date examples are:\n"
+                                              "\n7/11(9:00-23:59)\n"
+                                              "8/11(noon)\n"
+                                              "9/11(13:00-18:45)\n" 
+                                              "Each date must be separated with ENTER from the next one."), inline=False)
         await ctx.send(embed=em)
 
     @help.command()
     async def show(self, ctx):
         em = discord.Embed(title="Show", description="Shows a sorted list of every date the user has entered so far", color=ctx.author.color)
         em.add_field(name="**Syntax**", value="!show", inline=False)
+        await ctx.send(embed=em)
+
+    @help.command()
+    async def stats(self, ctx):
+        em = discord.Embed(title="Stats", description="Shows more information about the users and all the entered dates", color=ctx.author.color)
+        em.add_field(name="**Syntax**", value="!stats", inline=False)
         await ctx.send(embed=em)
 
     @help.command()
