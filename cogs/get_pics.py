424067e91974f16ccfc18128028234de10f6d0f8
pgeorgoulis <porfyrios21@gmail.com>
2023-03-21 21:00:22 +0200

    Added slash commands

--- cogs/get_pics.py
+++ cogs/get_pics.py
@@ -1,5 +1,6 @@
 from discord.ext.commands import command
 from discord.ext.commands import Cog
+from discord import app_commands
 import discord
 import utils
 
@@ -8,22 +9,22 @@ class Pictures_in_Channel(Cog):
     def __init__(self, client):
         self.client = client
 
-    @command(name = "pics", aliases=["pic", "get_pictures", "get_pics", "pictures"], pass_context = True)
-    async def pics(self, ctx, limit = None):
+    @app_commands.command(name="pics", description="Posts the images contained in the last `limit` messages. The default value for limit is 10000")
+    async def pics(self, interaction: discord.Interaction, limit: int= None):
         if limit == None:
             limit = 10000
         else:
             if utils.is_number(limit):
                 limit = int(limit)
             else:
-                await ctx.send("Warning: Given limit value is not valid. The command will proceed with the default limit value")
+                await interaction.channel.send("Warning: Given limit value is not valid. The command will proceed with the default limit value")
                 limit = 10000
 
         counter = 0
         pic_counter =0
         pic_list = []
-        await ctx.send("Searching...", delete_after=15)
-        async for message in ctx.channel.history(limit=limit):
+        await interaction.channel.send("Searching...", delete_after=15)
+        async for message in interaction.channel.history(limit=limit):
             counter += 1
             atm_list = message.attachments
             if len(atm_list) != 0:
@@ -33,7 +34,7 @@ class Pictures_in_Channel(Cog):
                         pic_counter += 1
                         pic_list.append(atm)
 
-        thread = await ctx.channel.create_thread(name="Channel images",
+        thread = await interaction.channel.create_thread(name="Channel images",
                                                     auto_archive_duration=60,
                                                     type = discord.ChannelType.public_thread)
         await thread.send("Behold! My pics.")
@@ -41,7 +42,7 @@ class Pictures_in_Channel(Cog):
         for pic in reversed(pic_list):
             await thread.send(pic.url)
 
-        await ctx.send(f'{pic_counter} pics found in the last {counter} messages', delete_after = 10)
+        await interaction.channel.send(f'{pic_counter} pics found in the last {counter} messages', delete_after = 10)
 
 async def setup(client):
     await client.add_cog(Pictures_in_Channel(client))
