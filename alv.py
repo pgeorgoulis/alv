3670ac1c39add20ecfbc1312571523a4c71304b3
pgeorgoulis <porfyrios21@gmail.com>
2023-03-21 17:38:46 +0200

    Added cog reload and basic slash commands

--- alv.py
+++ alv.py
@@ -3,12 +3,15 @@ import os
 import asyncio
 import random
 import discord
+import discord.ext
 from dotenv import load_dotenv
 from discord.ext import commands
+from discord.ext.commands import MissingPermissions
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-#GUILD = os.getenv('DISCORD_GUILD')
+GUILD = os.getenv('SERVER_ID')
 
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix='!', intents=intents)
@@ -27,11 +30,15 @@ async def main():
 
 
 #Events
+#sync the slash command
 @client.event
 async def on_ready():
     print(f"discord.py {discord.__version__}\n")
 
+    synced = await client.tree.sync()
+
     print("[+] Alv is running...")
+    print("[+] Slash Commands synced: "+ str(len(synced))+" commands")
     allowed_mentions = discord.AllowedMentions(everyone=True)
     channel = client.get_channel(1057014585388978196) #general
     #await channel.send("@everyone Alv is now online.", allowed_mentions=allowed_mentions)
@@ -89,5 +96,65 @@ async def on_message(message):
             picture = discord.File(f)
             await message.channel.send(file=picture)
     await client.process_commands(message)
+
+@client.tree.command(name="ping", description="Get the bot's current latency!")
+async def ping(interaction: discord.Interaction):
+    await interaction.response.send_message(f'{int(client.latency * 1000)} ms.')
+
+
+@client.tree.command(name="cog", description="Load, Unload or Reload a cog file.")
+@commands.guild_only()
+@commands.is_owner()
+async def cog(interaction: discord.Interaction, action: str, path: str=None):
+    folders = [filename for filename in os.listdir('./cogs')]
+
+    if path:
+        if path.endswith(".py"):
+            path = path[:-3]
+
+        cog_names = []
+        for filename in os.listdir(f'./cogs'):
+            if filename.endswith(".py"):
+                cog_names.append(filename[:-3])
+        
+        if path in cog_names:
+            cog_file = f'cogs.{path}'
+            if action == "reload":
+                await client.unload_extension(cog_file)
+                await client.load_extension(cog_file)
+                await interaction.response.send_message(f'RELOADED: cogs.{path}')
+            elif action == "load":
+                await client.load_extension(cog_file)
+                await interaction.response.send_message(f'LOADED: cogs.{path}')
+            elif action == "unload":
+                await client.unload_extension(cog_file)
+                await interaction.response.send_message(f'UNLOADED: cogs.{path}')
+            else:
+                await interaction.response.send_message("Incorrect argument received: [action]") 
+        else: 
+            await interaction.response.send_message(f'There is no cog named: ```{path}``` ')                    
+
+    else:
+        if action == "reload":
+            counter = 0
+            for filename in os.listdir(f'./cogs'):
+                if filename.endswith(".py"):
+                    counter += 1
+                    file = f'cogs.{filename[:-3]}'
+                    await client.unload_extension(file)
+                    await client.load_extension(file)
+            await interaction.response.send_message(f'RELOADED: {counter} Cogs')
+        elif action == "load" or action == "unload":
+            await interaction.response.send_message("You must provide a valid path in order to use ```load``` or ```unload``` actions")
+        else:
+            await interaction.response.send_message("Incorrect argument received: [action]")
+
+
+@cog.error
+async def cog_error(self, ctx, error):
+    if isinstance(error, MissingPermissions):
+        await ctx.send("This command is only available to sigma males")
+    else:
+        raise error
 
 asyncio.run(main())
