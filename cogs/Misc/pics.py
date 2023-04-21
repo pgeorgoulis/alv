2dcebedb65f0d22bcd66ff4c874d78aa72b76a64
pgeorgoulis <porfyrios21@gmail.com>
2023-04-21 15:15:25 +0300

    Added files command

--- cogs/Misc/pics.py
+++ cogs/Misc/pics.py
@@ -42,5 +42,34 @@ class Pictures_in_Channel(Cog):
 
         await interaction.channel.send(f'{pic_counter} pics found in the last {counter} messages', delete_after = 10)
 
+    @app_commands.command(name="files", description="Posts the files contained in the last `limit` messages. The default value for limit is 10000")
+    async def files(self, interaction: discord.Interaction, limit: int= None):
+        if limit == None:
+            limit = 10000
+
+        counter = 0
+        pic_counter =0
+        pic_list = []
+        await interaction.response.send_message("Searching...")
+        async for message in interaction.channel.history(limit=limit):
+            counter += 1
+            atm_list = message.attachments
+            if len(atm_list) != 0:
+                for atm in atm_list:
+                    atm_type = atm.content_type.split("/")[0]
+                    if atm_type == 'application' or atm_type == 'text':
+                        pic_counter += 1
+                        pic_list.append(atm)
+
+        thread = await interaction.channel.create_thread(name="Channel images",
+                                                    auto_archive_duration=60,
+                                                    type = discord.ChannelType.public_thread)
+        await thread.send("Behold! My pics.")
+        i = 0
+        for pic in reversed(pic_list):
+            await thread.send(pic.url)
+
+        await interaction.channel.send(f'{pic_counter} pics found in the last {counter} messages', delete_after = 10)
+
 async def setup(client):
     await client.add_cog(Pictures_in_Channel(client))
